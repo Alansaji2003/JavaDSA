bdcdc48e083665784207b0f97360aabc9c98a9b3
Alan Saji <rockstaralansaji@gmail.com>
2024-03-08 23:01:53 +0530

    factorial

---
+++ DSA/Recursion/src/alan/factorial.java
@@ -0,0 +1,17 @@
+package alan;
+
+public class factorial {
+
+
+    public static int fact(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        return n * fact(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        System.out.println(fact(n));
+    }
+}
