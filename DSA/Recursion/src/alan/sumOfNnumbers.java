02280fe3ab0e38696211346c1e4db2ca291aec94
Alan Saji <rockstaralansaji@gmail.com>
2024-03-08 22:29:29 +0530

    Test commit from Intellij

--- DSA/Recursion/src/alan/sumOfNnumbers.java
+++ DSA/Recursion/src/alan/sumOfNnumbers.java
@@ -1,7 +1,15 @@
 package alan;
 
 public class sumOfNnumbers {
+
+    public static void findSum(int i, int sum){
+
+    }
+
     public static void main(String[] args) {
-        
+
+        int n = 8;
+        int i = 1;
+        findSum(i, n);
     }
 }
