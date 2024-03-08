4e47f8df358984a940960b68600fefc9ad5f854a
Alan Saji <rockstaralansaji@gmail.com>
2024-03-08 22:57:41 +0530

    sumofnumbers

--- DSA/Recursion/src/alan/sumOfNnumbers.java
+++ DSA/Recursion/src/alan/sumOfNnumbers.java
@@ -2,19 +2,30 @@ package alan;
 
 public class sumOfNnumbers {
 
+    //two ways: parameterized and functional
+    // parameterized
     public static void findSum(int i, int sum){
-        if(i < 0){
-            System.out.println(sum);
+        if(i < 1){
+            System.out.println("Using parameterized recursion :"+sum);
             return;
         }
 
         findSum(i-1,sum+i);
     }
 
+    //fucntional
+    public static int findSum(int n){
+        if(n == 0){
+            return 0;
+        }
+        return n + findSum(n-1);
+
+    }
     public static void main(String[] args) {
 
         int n = 3;
-        int i = 1;
+
         findSum(n, 0);
+        System.out.println("From functional recursion :"+findSum(n));
     }
 }
